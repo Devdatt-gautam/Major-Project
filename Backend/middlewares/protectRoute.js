41652191ae70617d244c13588707e0ec432298ea
Devdatt Gautam <devg26092001@gmail.com>
2024-02-14 21:24:32 +0530

    added token error

--- Backend/middlewares/protectRoute.js
+++ Backend/middlewares/protectRoute.js
@@ -5,7 +5,7 @@ const protectRoute = async (req, res, next) => {
     const token = await req.cookies.jwt;
 
     if (!token) {
-      return res.status(401).json({ error: "unauthorised" });
+      return res.status(401).json({ error: "unauthorised no token" });
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.userId).select("-password");
