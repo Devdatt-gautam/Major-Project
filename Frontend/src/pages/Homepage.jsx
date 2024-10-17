8e0b8ef06ac63732560ee73db806fde8876f2224
Devdatt Gautam <devg26092001@gmail.com>
2024-10-17 16:59:03 +0530

    remove suggested user from mobile

--- Frontend/src/pages/Homepage.jsx
+++ Frontend/src/pages/Homepage.jsx
@@ -53,7 +53,7 @@ const Homepage = () => {
           return <Post key={post._id} post={post} postedBy={post.postedBy} />;
         })}
       </Box>
-      <Box flex={30}>
+      <Box flex={30} display={{ base: "none", md: "block" }}>
         <SuggestedUsers />
       </Box>
     </Flex>
